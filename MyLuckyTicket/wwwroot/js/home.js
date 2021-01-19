498d7045b352ab7389c583cc245084cdd83f9625
toandk <khactoan96@gmail.com>
2021-01-19 22:54:34 +0700

    Edit css

--- MyLuckyTicket/wwwroot/js/home.js
+++ MyLuckyTicket/wwwroot/js/home.js
@@ -11,6 +11,10 @@
 
     function searchTicketNumber() {
         var search_number = $("#seach-ticket").siblings("input")[0].value.trim();
+        if (search_number == "") {
+            $(".ticket-number").css("background-color", "");
+            return $("#congrat").html("Congrats:");
+        }
         var win_the_prize = false;
 
         $.each($(".ticket-number"), function (index, element) {
@@ -18,7 +22,7 @@
             if (Number(search_number) == ticket_number) {
                 win_the_prize = true;
                 var account = $("#ticket-" + ticket_number).closest("tr").find(".account").html();
-                $("#congrat").html("Congrats: " + account);
+                $("#congrat").html("Congrats: " + account + "^^! - " + ticket_number);
                 $("#ticket-" + ticket_number).css("background-color", "yellow");
             } else {
                 $("#ticket-" + ticket_number).css("background-color", "");
